13f0c7a4fbb4eeb69b897d5224e65d1d0770fc73
Shefaa <shefaasomar@gmail.com>
2022-06-06 08:09:29 +0300

    Splitted each test into separate test case

--- cypress/integration/stubbing.js
+++ cypress/integration/stubbing.js
@@ -2,16 +2,12 @@ describe("Stubbing a request and logging the intercepted request data", () => {
   it("stubbing a request", () => {
     cy.visit("https://jsonplaceholder.typicode.com/");
 
-    cy.intercept({
-      path: "/posts",
-    }, (req) => {
-      // req.reply(
-      //   {
-      //     body: "hello world!",
-      //     statusCode: 201,
-      //   }
-      // )
-    }).as("posts");
+    cy.intercept(
+      {
+        path: "/posts",
+      },
+      () => {}
+    ).as("posts");
 
     cy.contains("/posts").click();
 
@@ -20,11 +16,35 @@ describe("Stubbing a request and logging the intercepted request data", () => {
       cy.log(JSON.stringify(interception));
 
       // accessing response body and logging specific info
-      cy.log(JSON.stringify(interception.response.body.length))
+      cy.log(JSON.stringify(interception.response.body.length));
 
       // assertion on body length
-      expect(interception.response.body).to.have.length(100)
+      expect(interception.response.body).to.have.length(100);
     });
-
   });
+
+  it("Mocking with intercept", () => {
+    cy.visit("https://jsonplaceholder.typicode.com/");
+
+    cy.intercept(
+      {
+        path: "/posts",
+      },
+      (req) => {
+        req.reply({
+          body: "hello world!",
+          statusCode: 201,
+        });
+      }
+    ).as("posts");
+
+    cy.contains("/posts").click();
+
+    cy.wait("@posts").then((interception) => {
+      // logging the whole interception
+      cy.log(JSON.stringify(interception));
+    });
+  });
+
+
 });
